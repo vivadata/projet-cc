dfe32a119dc52838a838a41979c499d88b4cb0a2
Nicolas Merinian <nicolas.merinian@gmail.com>
2025-11-18 15:57:21 +0100

    feat/cleaning-table-pluvio: cast AAAAMM from str to date

--- models/staging/stg_mensq_pluviometrie.sql
+++ models/staging/stg_mensq_pluviometrie.sql
@@ -1,8 +1,9 @@
 -- remove RR_ME column due to 98.6% of NULL values
+-- parse date AAAAMM
 
 SELECT 
   NUM_POSTE,
-  AAAAMM,
+  PARSE_DATE('%Y%m%d',CONCAT(AAAAMM,'01'))
   RR,
   QRR,
   NBRR,
@@ -14,5 +15,5 @@ SELECT
   NBJRR10,
   NBJRR30,
   NBJRR50,
-  NBJRR100
+  NBJRR100 
 from {{ source('data_meteofrance', 'MENSQ_974_1900-2025') }}
