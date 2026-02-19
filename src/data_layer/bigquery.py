3f15816069c4f3b3b47b58dac524f6aae5316c82
Nicolas Merinian <nicolas.merinian@gmail.com>
2026-02-19 22:43:49 +0100

    tech/dbt-add-marts: deleted unused queries

--- src/data_layer/bigquery.py
+++ src/data_layer/bigquery.py
@@ -20,51 +20,6 @@ def get_bq_client():
 def run_query(sql: str):
     client = get_bq_client()
     return client.query(sql).to_dataframe()
-    
-# Exemple de fonction qui fait un truc
-def get_todo1():
-    return run_query(f"""
-        WITH CTE AS (
-        SELECT 
-            ANNEE AS annee,
-            MOIS AS mois,
-            -- On garde AAAAMM pour le tri ou l'affichage chronologique
-            AAAAMM AS date_key, 
-
-            -- Somme des précipitations pour la période donnée
-            AVG(RR) AS Cumul_Mensuel_Pluie_Total,
-
-            --  'NBRR', c'est le nombre de jours de pluie
-
-            -- RRAB précipitation maximale tombée en 24 heures au cours du mois (Average)
-            AVG(RRAB) AS Cumul_MAxi_par_mois,
-
-            -- Nombre de jours > 100mm
-            AVG(NBJRR100) AS Nb_Jours_Sup_100mm
-
-        FROM `cc-reunion.data_meteofrance.stg_mensq_pluviometrie`
-        -- Indispensable pour fusionner les données de toutes les stations par mois
-        GROUP BY 
-            ANNEE, 
-            MOIS, 
-            AAAAMM
-
-        -- Tri par ordre chronologique (ce qui réglera aussi ton problème de tri dans le graph)
-        ORDER BY 
-            AAAAMM ASC
-        )
-
-        SELECT *
-        FROM CTE
-        WHERE Nb_Jours_Sup_100mm > 1
-    """)
-
-def get_data():
-    return run_query("""
-                     SELECT ANNEE, moy_nuits_ge_20 
-                     FROM `cc-reunion.data_meteofrance.int_mensq_temperatures_sup_20deg`
-                     ORDER BY ANNEE ASC
-    """)
 
 def get_nb_moy_nuits_sup_20deg():
     return run_query("""
